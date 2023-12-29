7b9f6d0a3943f3fed9b7f300157ad30a68419c98
Zhicheng-Lu <andylu.zc@gmail.com>
2023-12-29 21:23:22 +1100

    segmentation GNN

--- modules/segmentation.py
+++ modules/segmentation.py
@@ -6,7 +6,7 @@ import numpy as np
 class Segmentation(nn.Module):
 	def __init__(self):
 		super(Segmentation, self).__init__()
-		self.f_size = 64
+		self.f_size = 32
 		self.down1 = nn.Sequential(
 			nn.Conv2d(in_channels=1, out_channels=self.f_size, kernel_size=(3,3), padding=(1,1)),
 			nn.ReLU(),
@@ -69,17 +69,32 @@ class Segmentation(nn.Module):
 			nn.ConvTranspose2d(in_channels=2*self.f_size, out_channels=self.f_size, kernel_size=(3,3), stride=(2,2), padding=(1,1), output_padding=(1,1)),
 			nn.ReLU()
 		)
-		self.conv = nn.Sequential(
-			nn.Conv2d(in_channels=2*self.f_size, out_channels=self.f_size, kernel_size=(3,3), padding=(1,1)),
-			nn.ReLU(),
+		# self.conv = nn.Sequential(
+		# 	nn.Conv2d(in_channels=2*self.f_size, out_channels=self.f_size, kernel_size=(3,3), padding=(1,1)),
+		# 	nn.ReLU(),
+		# 	nn.Conv2d(in_channels=self.f_size, out_channels=self.f_size, kernel_size=(3,3), padding=(1,1)),
+		# 	nn.ReLU(),
+		# 	nn.Conv2d(in_channels=self.f_size, out_channels=2, kernel_size=(1,1))
+		# )
+		self.aggr_1 = nn.Sequential(
+			nn.Conv2d(in_channels=2*self.f_size, out_channels=2*self.f_size, kernel_size=(3,3), padding=(1,1)),
+			nn.ReLU()
+		)
+		self.update_1 = nn.Sequential(
+			nn.Conv2d(in_channels=4*self.f_size, out_channels=self.f_size, kernel_size=(3,3), padding=(1,1)),
+			nn.ReLU()
+		)
+		self.aggr_2 = nn.Sequential(
 			nn.Conv2d(in_channels=self.f_size, out_channels=self.f_size, kernel_size=(3,3), padding=(1,1)),
-			nn.ReLU(),
+			nn.ReLU()
+		)
+		self.update_2 = nn.Sequential(
+			nn.Conv2d(in_channels=2*self.f_size, out_channels=self.f_size, kernel_size=(3,3), padding=(1,1)),
+			nn.ReLU()
+		)
+		self.conv = nn.Sequential(
 			nn.Conv2d(in_channels=self.f_size, out_channels=2, kernel_size=(1,1))
 		)
-		# self.dense = nn.Sequential(
-		# 	nn.Linear(in_features=self.f_size, out_features=2),
-		# 	nn.Softmax(dim=3)
-		# )
 		
 
 	def forward(self, device, patient_range, cts):
@@ -96,10 +111,10 @@ class Segmentation(nn.Module):
 		up3_cat = torch.cat((down2, up3), dim=1)
 		up4 = self.up4(up3_cat)
 		up4_cat = torch.cat((down1, up4), dim=1)
-		output = self.conv(up4_cat)
-		# conv = torch.moveaxis(conv, 1, 3)
-		# output = self.dense(conv)
-		# output = torch.moveaxis(output, 3, 1)
+		# output = self.conv(up4_cat)
+		gnn1 = self.GNN_layer(device, patient_range, up4_cat, self.aggr_1, self.update_1)
+		gnn2 = self.GNN_layer(device, patient_range, gnn1, self.aggr_2, self.update_2)
+		output = self.conv(gnn2)
 
 		# print(cts.shape)
 		# print(down1.shape)
@@ -117,5 +132,45 @@ class Segmentation(nn.Module):
 		# print(up4_cat.shape)
 		# print(conv.shape)
 		# print(output.shape)
+		# print(up4_cat.shape)
+		# print(gnn1.shape)
+		# print(gnn2.shape)
+		# print(gnn3.shape)
 
 		return output
+
+
+	def GNN_layer(self, device, patient_range, in_features, aggr, update):
+		features = in_features[:, None, :, :, :]
+		features = features.repeat(1,2,1,1,1)
+
+		for i,end in enumerate(patient_range):
+			# Get start and end index for certain patient
+			if i == 0:
+				start = 0
+			else:
+				start = patient_range[i-1]
+
+			for n1 in range(start, end):
+				similarities = []
+				# Find similarities of all pairs
+				for n2 in range(start, end):
+					if n1 == n2:
+						continue
+					similarities.append((n2, nn.functional.cosine_similarity(torch.flatten(features[n1,0]), torch.flatten(features[n2,0]), dim=0)))
+				# Sort and slice top 5
+				similarities = sorted(similarities, key=lambda x: x[1], reverse=True)
+				if len(similarities) < 3:
+					knn = [similarity[0] for similarity in similarities]
+				else:
+					knn = [similarities[i][0] for i in range(len(similarities)) if i<3]
+				knn = features[knn,0]
+				knn = aggr(knn)
+				knn = torch.mean(knn, dim=0, keepdim=False)
+				features[n1,1] = knn
+
+		features = torch.reshape(features, (features.shape[0], features.shape[1]*features.shape[2], features.shape[3], features.shape[4]))
+		features = update(features)
+
+
+		return features
--- train.py
+++ train.py
@@ -30,6 +30,7 @@ def main(argv):
 	# Get cpu or gpu device for training.
 	device = "cuda" if torch.cuda.is_available() else "cpu"
 	print(f"Using {device} device")
+	torch.cuda.empty_cache()
 	time = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
 
 	if task == 'segmentation' and (step == 'train' or step == 'training'):
@@ -42,17 +43,11 @@ def segmentation_train(data_reader, device, time):
 	entropy_loss_fn = nn.CrossEntropyLoss()
 	dice_loss_fn = dice_loss()
 	model = Segmentation().to(device)
-	print(model)
 
 	optimizer = torch.optim.RMSprop(model.parameters(), lr=1e-5)
 
 	for epoch in range(data_reader.epochs):
 		patient_range, cts, masks = data_reader.read_in_batch('segmentation', 'train')
-		# masks = masks[:,:,:,None]
-		# for i in range(len(cts)):
-		# 	cv2.imwrite(f'temp/{i}_ct.png', cts[i]*255)
-		# 	cv2.imwrite(f'temp/{i}_mask.png', masks[i]*255)
-		# return
 		cts = torch.from_numpy(np.moveaxis(cts, 3, 1))
 		masks = torch.from_numpy(masks)
 		for iteration in range(data_reader.iterations):
@@ -77,30 +72,37 @@ def segmentation_train(data_reader, device, time):
 
 
 def segmentation_test(data_reader, device, time):
-	loss_fn = nn.CrossEntropyLoss()
+	entropy_loss_fn = nn.CrossEntropyLoss()
+	dice_loss_fn = dice_loss()
+
 	model = Segmentation()
 	model.load_state_dict(torch.load("checkpoints/segmentation_model.pt"), strict=False)
 	model = model.to(device)
 
 	patient_range, cts, masks = data_reader.read_in_batch('segmentation', 'test')
+	# Print out input
+	# masks = masks[:,:,:,None]
+	for i in range(len(cts)):
+		cv2.imwrite(f'test/{i}_ct.png', cts[i]*255)
+		cv2.imwrite(f'test/{i}_mask.png', masks[i]*255)
+
+	# Make prediction
 	cts = torch.from_numpy(np.moveaxis(cts, 3, 1))
 	cts = cts.to(device=device, dtype=torch.float)
 	pred = model(device, patient_range, cts)
 	masks = torch.from_numpy(masks)
 	masks = masks.type(torch.cuda.LongTensor)
 	masks.to(device)
-	loss = loss_fn(pred, masks)
+	loss = entropy_loss_fn(pred, masks) + dice_loss_fn(pred, masks)
 	loss = loss.item()
 	print(f"loss: {loss:>7f}")
 
 	output = pred.cpu().detach().numpy()
 	output = np.argmax(output, axis=1)
-	print(np.max(output))
 	output = output[:, :, :, None]
-	# output = np.repeat(output, 3, axis=3)
 	output = output * 255
 	for i,img in enumerate(output):
-		cv2.imwrite('test/{}.jpg'.format(i), img)
+		cv2.imwrite('test/{}_output.jpg'.format(i), img)
 
 
 class dice_loss(torch.nn.Module):
